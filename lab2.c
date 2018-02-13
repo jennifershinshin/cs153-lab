e501801fea658c311ffcce1f6993139b6d3e9308
Jennifer Shin <jshin026@sledge.cs.ucr.edu>
2018-02-13 11:01:54 -0800

    fixed lab2.c formatting. could use some more work though

--- lab2.c
+++ lab2.c
@@ -17,41 +17,38 @@ int main(int argc, char *argv[])
 		 
     // use this part to test the priority scheduler. Assuming that the priorities range between range between 0 to 63
     //     // 0 is the highest priority. All processes have a default priority of 20 
-    //
-           int pid;
-             int i,j,k;
+
+     int pid;
+     int i,j,k;
                
-                   printf(1, "\n  Step 2: testing the priority scheduler and setpriority(int priority)) systema call:\n");
-                       printf(1, "\n  Step 2: Assuming that the priorities range between range between 0 to 31\n");
-                           printf(1, "\n  Step 2: 0 is the highest priority. All processes have a default priority of 10\n");
-                               printf(1, "\n  Step 2: The parent processes will switch to priority 0\n");
-                                   setpriority(0);
-                                       for (i = 0; i <  3; i++) {
-                                       	pid = fork();
-                                       		if (pid > 0 ) {
-                                       				continue;}
-                                       					else if ( pid == 0) {
-    
-                                       							setpriority(30-10*i);	
-                                       									for (j=0;j<50000;j++) {
-                                       												for(k=0;k<10000;k++) {
-                                       																asm("nop"); }}
-                                       																		printf(1, "\n child# %d with priority %d has finished! \n",getpid(),30-10*i);		
-                                       																				
-                                       																				        }
-                                       																				                else {
-                                       																				                			printf(2," \n Error \n");
-                                       																				                						
-                                       																				                						        }
-                                       																				                						        	}
-    
-                                       																				                						        		if(pid > 0) {
-                                       																				                						        				for (i = 0; i <  3; i++) {
-                                       																				                						        							wait();
-    
-                                       																				                						        									}
-                                       																				                						        									                     printf(1,"\n if processes with highest priority finished first then its correct \n");
-                                       																				                						        									                     }
-                                       																				                						        									                     			
-                                       																				                						        									                     				return 0;}
-    
+	printf(1, "\n  Step 2: testing the priority scheduler and setpriority(int priority)) systema call:\n");
+	printf(1, "\n  Step 2: Assuming that the priorities range between range between 0 to 31\n");
+	printf(1, "\n  Step 2: 0 is the highest priority. All processes have a default priority of 10\n");
+	printf(1, "\n  Step 2: The parent processes will switch to priority 0\n");
+	setpriority(0);
+	for (i = 0; i <  3; i++) {
+       		pid = fork();
+		if (pid > 0 ) {
+			continue;}
+		else if ( pid == 0) {
+			setpriority(30-10*i);	
+			for (j=0;j<50000;j++) {
+				for(k=0;k<10000;k++) {
+					asm("nop"); }}
+			printf(1, "\n child# %d with priority %d has finished! \n",getpid(),30-10*i);
+			exit();
+       		}
+       		else {
+			printf(2," \n Error \n");
+        	}
+		}
+
+
+		if(pid > 0) {
+			for (i = 0; i <  3; i++) {
+				wait();
+			}
+                     printf(1,"\n if processes with highest priority finished first then its correct \n");
+		}
+	return 0;}
+
